b17799f77ff8a78b1ebb47278a141a4448b9b957
Prince <pm606433@gmail.com>
2025-09-15 05:14:15 +0530

    new code

--- recursion.cpp
+++ recursion.cpp
@@ -3,14 +3,34 @@ using namespace std;
 
 void printNums(int n){
     if(n==1){
-        cout << 1<<  endl;
+        cout << 1<<" " ;
         return;
     }
-    cout<< n <<" ";
-    // printNums(n-1);
+    cout<< n <<" ";// ye pehle hi print hota hy  
+
+    printNums(n-1);
+    cout<< n <<" "; // ye jab backtrack hone lagta hy tab print hota hy 
     
 }
+
+int  nFact(int n){
+
+    if (n==1){
+        return 1;
+    }
+    return n* nFact(n-1);
+}
+int sumNnumber(int n){
+    if (n==1){
+        return 1;
+    }
+    return n + sumNnumber(n-1);
+}
 int main(){
-    printNums(6);
+    int fact=1;
+    // printNums(6);
+    cout<<nFact(5) << endl;
+    cout<<sumNnumber(5
+    ) << endl;
     return 0;
 }
